0f5492259198e3dd4e5b3235151eea9860b5d4ec
William McDonald <wmcdonald2k@gmail.com>
2018-09-25 22:05:40 -0700

    Github Page Jquery overhaul.

---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.header').load("templates/header.html");
+});
+
+$(document).ready(function(){
+  $('.contact').load("templates/contact.html");
+});
+
+$(document).ready(function(){
+  $('.education').load("templates/education.html");
+});
